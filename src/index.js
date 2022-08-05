2ea3774d34a1734bbefb944e2ffb48d504027c04
manassinghdewari <mnsdewari@gmail.com>
2022-08-06 00:12:38 +0530

    basic setup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.css'
+import App from './components/App';
+import { StyledEngineProvider } from '@mui/material';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <StyledEngineProvider injectFirst>
+        <App />
+    </StyledEngineProvider>
+    
+);
