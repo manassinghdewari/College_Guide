fa61198a25d2d1f4ec6d68df8200252a88ebcc1d
UttamBindal <ping.uttambindal@gmail.com>
2022-08-09 00:22:33 +0530

    Added cards

---
+++ src/components/CollegeCard.jsx
@@ -0,0 +1,38 @@
+import React from "react"
+const CollegeCard=()=>
+(
+    <>
+        <div class="card rounded-3 wid22">
+    <img class="card-img rounded-3 hei18" src="https://www.smvit.in/Default/ClientPanel/media/smvit_slider1.jpg" alt="Sir M. Visversaraya Institute of Tecnology"  />
+    <div class="card-img-overlay rounded-3 ">
+    <div class="row">
+      <div class="col"><a href="/" class="btn2 btn btn-primary rounded-3">Brochure</a></div>
+      <div class="col tar">
+        <button type="button" class="btn lik btn-link"><span class="bi bi-heart"></span></button>
+      </div>
+    </div>
+    <div class="ran">
+    <span class="rank rounded-4">
+      Rank:250
+    </span>
+  </div>
+    </div>
+    <div class="card-body">
+      
+      <div class="row p4"><div class="col">Bangalore,IN</div><div class="col col-4"><a href="/" class="btn3 btn btn-primary rounded-3">Compare</a></div></div>
+      <div>
+        <div class="title row p4"><div class="col-5"><img src="https://images.shiksha.com/mediadata/images/1651569550phpDG50QS.jpeg" alt="Logo" width="75" height="75" /></div>
+        <div class="col">
+        Sir M. Visversaraya Institute of Tecnology</div>
+      </div>
+    </div>
+      <div class="row p4"><div class="col acc">AICTE,UGC Approved</div><div class="col est">ESTD.1935</div></div>
+      <hr />
+      <div class="row p4"><div class="col rate">&#9733; Rating:</div><div class="col">8/10</div></div>
+    </div>
+  </div>
+            
+        </>
+        )
+        
+        export default CollegeCard;
