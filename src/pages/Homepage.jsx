3cdef9cf47b84936a6799d216ac9d01b599f5bb7
MANAS DEWARI <mnsdewari@gmail.com>
2022-08-17 15:32:06 +0530

    Revert "Sample"

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import Navbar from '../components/Navbar'
+import ImageSlider from '../components/imageSlider/ImageSlider'
 import CustomPaginationActionsTable from '../components/Table'
-import CourseCardSlider from '../components/CourseCardSlider/CourseCardSlider'
-import NewsCardSlider from '../components/NewsCardSlider/NewsCardSlider';
-import Banner from '../components/Banner/Banner';
-import Footer from '../components/Footer/Footer';
+
 const Homepage=()=>{
     return(
         <>
             <Navbar />
-            <Banner />
-            <NewsCardSlider />
+            <ImageSlider />
             <CustomPaginationActionsTable />
-            <CourseCardSlider />
-            <Footer />
         </>
     )
 }
