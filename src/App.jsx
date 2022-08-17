e0eeadd6d396d7d0932fc529c0b3a280b6b501be
manassinghdewari <mnsdewari@gmail.com>
2022-08-17 16:32:46 +0530

    merge fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,10 @@
-<<<<<<< HEAD
 
-import Homepage from './pages/Homepage';
-
-  import InstitutePage from './pages/InstituePage';
-const App =()=>{
-  return(
-    <>
-      <Homepage/>
-
-=======
 import React from 'react';
 import Homepage from './pages/Homepage';
 const App =()=>{
   return(
     <>
       <Homepage/>
->>>>>>> 781aeeb00d81b5a0cff8199b82a4c603c1555537
     </>
   )
 }
