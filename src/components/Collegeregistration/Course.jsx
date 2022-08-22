309f827a2539ae6e031bd94868f0516321bae37e
UttamBindal <ping.uttambindal@gmail.com>
2022-08-22 18:53:48 +0530

    Fixed Bug :Compare Button in College Card not working Centered the logo and fixed a mereg conflict error file

--- src/components/Collegeregistration/Course.jsx
+++ src/components/Collegeregistration/Course.jsx
@@ -1,4 +1,3 @@
-
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import InputLabel from '@mui/material/InputLabel';
@@ -14,25 +13,8 @@ import { v4 as uuidv4 } from 'uuid';
 import BranchDetails from './BranchDetails';
 
  function CourseSelect() {
-
-import * as React from "react";
-import Box from "@mui/material/Box";
-import InputLabel from "@mui/material/InputLabel";
-import Container from "@material-ui/core/Container";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select from "@mui/material/Select";
-import { ClassNames } from "@emotion/react";
-import { IconButton, Typography } from "@mui/material";
-import RemoveIcon from "@material-ui/icons/Remove";
-import AddIcon from "@material-ui/icons/Add";
-import { v4 as uuidv4 } from "uuid";
-import BranchDetails from "./BranchDetails";
-
-function BasicSelect() {
-
   const [Courses, setCourses] = React.useState([
-    { id: uuidv4(), CourseName: "" },
+    { id: uuidv4(),  CourseName: ''},
   ]);
   const courses = [
     { value: 'B.Tech', label: 'B.Tech' },
@@ -40,51 +22,26 @@ function BasicSelect() {
     { value: 'M.Tech', label: 'M.Tech' }
   ]
   const handleChangeInput = (id, event) => {
-    const newCourses = Courses.map((i) => {
-      if (id === i.id) {
-        i[event.target.name] = event.target.value;
-      }
-      return i;
-    });
+    const newCourses = Courses.map(i => {
+        if (id === i.id) {
+            i[event.target.name] = event.target.value
+        }
+        return i;
+    })
 
     setCourses(newCourses);
-  };
-  const handleAddFields = () => {
-    setCourses([...Courses, { id: uuidv4(), CourseName: "" }]);
-  };
+}
+const handleAddFields = () => {
+    setCourses([...Courses, { id: uuidv4(), CourseName: '' }])
+}
 
-  const handleRemoveFields = (id) => {
+const handleRemoveFields = id => {
     const values = [...Courses];
-    values.splice(
-      values.findIndex((value) => value.id === id),
-      1
-    );
+    values.splice(values.findIndex(value => value.id === id), 1);
     setCourses(values);
-  };
+}
 
   return (
-    <div className="box">
-      <Container alignItems="center">
-        <h1>Enter Course Details</h1>
-        <form className={ClassNames.root}>
-          {Courses.map((Course) => (
-            <span key={Course.id}>
-              <div>
-                <div className="addremove">
-                  <Typography variant="h6" display="block" gutterBottom>
-                    Add/Remove Course
-                  </Typography>
-                  <IconButton
-                    disabled={Courses.length === 1}
-                    onClick={() => handleRemoveFields(Course.id)}
-                  >
-                    <RemoveIcon />
-                  </IconButton>
-                  <IconButton onClick={handleAddFields}>
-                    <AddIcon />
-                  </IconButton>
-                </div>
-
 
     <div className ='box'>
         <Container alignItems ="center">
@@ -129,34 +86,6 @@ function BasicSelect() {
 </Container>
 </div>
    
-
-                <Box sx={{ maxWidth: 120 }}>
-                  <FormControl fullWidth>
-                    <InputLabel id="demo-simple-select-label">
-                      Course
-                    </InputLabel>
-                    <Select
-                      labelId="demo-simple-select-label"
-                      id="demo-simple-select"
-                      name="Course"
-                      value={Course.name}
-                      label="Course"
-                      onChange={(event) => handleChangeInput(Course.id, event)}
-                    >
-                      <MenuItem value={10}>Engineering</MenuItem>
-                      <MenuItem value={20}>Medical</MenuItem>
-                      <MenuItem value={30}>Arts</MenuItem>
-                    </Select>
-                    <BranchDetails />
-                  </FormControl>
-                </Box>
-              </div>
-            </span>
-          ))}
-        </form>
-      </Container>
-    </div>
-
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ const NavScrollExample = () => {
     <Navbar bg="primary" variant="dark" expand="lg">
       <Container fluid>
         <Navbar.Brand className="font-bold" href="#">
-          <SchoolIcon className="mr-5 ml-2" /> College Guide
+          <SchoolIcon className="mr-5 ml-2 -mt-1" /> College Guide
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
